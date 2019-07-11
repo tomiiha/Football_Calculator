c12a5f4fcb7a4567087c4b60cbb65c87a86cfdc2
thamyiha <52747351+thamyiha@users.noreply.github.com>
2019-07-11 12:42:57 +0100

    Update footiescrape.py

--- footiescrape.py
+++ footiescrape.py
@@ -8,13 +8,21 @@ import requests as reqs
 import xlsxwriter as xsl
 
 # Load data file to use
-workbook = xsl.Workbook('Ndata1819.xlsx')
+workbookname = 'Ndata1819.xlsx'
+workbook = xsl.Workbook(workbookname)
 worksheet = workbook.add_worksheet()
 
+# Status notice
+print("Workbook " + str(workbookname) + " created")
+
 # Capture website
-page = reqs.get("https://fbref.com/en/squads/986a26c1/Northampton-Town")
+pagetoparse = 'https://fbref.com/en/squads/986a26c1/Northampton-Town'
+page = reqs.get(pagetoparse)
 status = page.status_code
 parsepage = bsoup(page.content, 'html.parser')
+
+# Status notice
+print("Parsing: " + str(pagetoparse))
 
 # Lists
 playerlist = []
@@ -26,6 +34,9 @@ gamesubslist = []
 minuteslist = []
 minutespgamelist = []
 
+# Status notice
+print("Creating dataset")
+
 # Elements to ultimately find in toparse - gen player list off of findplayers
 # Actual toparse list below, enact for full dataset build
 #toparse = ["player","position","age","games","game_starts","game_subs","minutes","minutes_per_game"]
@@ -35,7 +46,9 @@ for player in findplayers:
     addplayer = player.find_next('a').get_text()
     if addplayer not in playerlist and addplayer != 'coverage note':
         playerlist.append(addplayer)
-print(playerlist)
+
+# Status notice
+print("Dataset created - adding to Excel sheet")
 
 # Data writing into excel file - insert lists to designated columns A1 onward
 startrow = 0
@@ -49,3 +62,6 @@ for player in addplayer:
     worksheet.write(startrow, startcol, player)
     startrow += 1
 workbook.close()
+
+# Status notice
+print("Excel sheet created and saved")
