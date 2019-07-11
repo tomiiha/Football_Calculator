5604327907d9b6349043e12318b31b17241d002b
thamyiha <52747351+thamyiha@users.noreply.github.com>
2019-07-11 15:21:48 +0100

    Create seasoncalcs.py

---
+++ seasoncalcs.py
@@ -0,0 +1,8 @@
+# Input for dataset - currently placeholder ifs
+seasons = ["18-19"]
+selection = input('What season?')
+selection = str(selection)
+if selection in seasons:
+    print('Yes')
+else :
+    print('Nah')
