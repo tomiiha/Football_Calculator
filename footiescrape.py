42c4cd2d84b7be05f279da968d44b6b24c5f1951
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-15 18:40:19 +0100

    Update footiescrape.py

--- footiescrape.py
+++ footiescrape.py
@@ -54,7 +54,7 @@ sot_list = []
 
 # Excel lists
 all_lists = [player_list,position_list,age_list,games_list,game_starts_list,game_subs_list,minutes_list,minutes_per_game_list, goals_list,assists_list,pens_made_list,pens_att_list,fouls_list,yellow_list,red_list,sot_list]
-to_parse = ["player","position","age","games","game_starts","game_subs","minutes","minutes_per_game","assists","pens_made","pens_att","fouls","cards_yellow","cards_red","shots_on_target"]
+to_parse = ["player","position","age","games","game_starts","game_subs","minutes","minutes_per_game","goals","assists","pens_made","pens_att","fouls","cards_yellow","cards_red","shots_on_target"]
 
 # Status notice
 print("Creating dataset")
@@ -66,7 +66,7 @@ for player in findplayers:
     addplayer = player.find_next('a').get_text()
     if addplayer not in player_list and addplayer != 'coverage note':
         player_list.append(addplayer)
-            
+
 # Create positionlist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'position'})
 for position in findinfo:
@@ -89,14 +89,14 @@ for games in findinfo:
         games_list.append(addgames)
 
 # Create gamestartlist - non-unique
-findinfo = parse_page.find_all('td',attrs={"data-stat":'game_starts'})
+findinfo = parse_page.find_all('td',attrs={"data-stat":'games_starts'})
 for gamestart in findinfo:
     addstart = gamestart.get_text()
     if addstart != 'coverage note':
         game_starts_list.append(addstart)
 
 # Create gamesubslist - non-unique
-findinfo = parse_page.find_all('td',attrs={"data-stat":'game_subs'})
+findinfo = parse_page.find_all('td',attrs={"data-stat":'games_subs'})
 for subs in findinfo:
     addgamesubs = subs.get_text()
     if addgamesubs != 'coverage note':
@@ -115,63 +115,63 @@ for minsp in findinfo:
     addminutespgame = minsp.get_text()
     if addminutespgame != 'coverage note':
         minutes_per_game_list.append(addminutespgame)
-        
+
 # Create goalslist - non-unique
-findinfo = parse_page.find_all('td',attrs={"data-stat":'assists'})
+findinfo = parse_page.find_all('td',attrs={"data-stat":'goals'})
 for goals in findinfo:
     addgoals = goals.get_text()
     if addgoals != 'coverage note':
         goals_list.append(addgoals)
-        
+
 # Create assistslist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'assists'})
 for assists in findinfo:
     addassists =  assists.get_text()
     if addassists != 'coverage note':
         assists_list.append(addassists)
-        
+
 # Create pensmadelist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'pens_made'})
 for pens in findinfo:
     addpensmade = pens.get_text()
     if addpensmade != 'coverage note':
         pens_made_list.append(addpensmade)
-        
+
 # Create pensattlist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'pens_att'})
 for pensa in findinfo:
     addpensatt = pensa.get_text()
     if addpensatt != 'coverage note':
         pens_att_list.append(addpensatt)
-        
+
 # Create foulslist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'fouls'})
 for fouls in findinfo:
     addfouls = fouls.get_text()
     if addfouls != 'coverage note':
         fouls_list.append(addfouls)
-        
+
 # Create yellowlist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'cards_yellow'})
 for yellow in findinfo:
     addyellow = yellow.get_text()
     if addyellow != 'coverage note':
         yellow_list.append(addyellow)
-        
+
 # Create redlist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'cards_red'})
 for red in findinfo:
     addred = red.get_text()
     if addred != 'coverage note':
         red_list.append(addred)
-        
+
 # Create sotlist - non-unique
 findinfo = parse_page.find_all('td',attrs={"data-stat":'shots_on_target'})
 for sot in findinfo:
     addsot = sot.get_text()
     if addsot != 'coverage note':
         sot_list.append(addsot)
-            
+
 # Status notice
 print("Dataset created - adding to Excel sheet")
 print("")
@@ -183,7 +183,7 @@ startcol = 0
 for header in to_parse:
     worksheet.write(startrow, startcol, header)
     startcol += 1
-    
+
 # Fill data points into set
 startrow = 1
 startcol = 0
