fa63d0d969a0453410cca505b73d51c726ba9bf4
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-15 13:05:49 +0100

    Update footiescrape.py

--- footiescrape.py
+++ footiescrape.py
@@ -51,8 +51,10 @@ foulslist = []
 yellowlist = []
 redlist = []
 sotlist = []
-all_lists = [playerlist,positionlist,agelist,gameslist,gamestartslist,gamesubslist,minuteslist,minutespgamelist, goalslist,assistslist,pensmadelist,pensattlist,foulslist,yellowlist,redlist,sotlist]
-toparse = ["player","position","age","games","game_starts","game_subs","minutes","minutes_per_game"]
+
+# If possible, simplify parse, and workbook creation with these
+#all_lists = [playerlist,positionlist,agelist,gameslist,gamestartslist,gamesubslist,minuteslist,minutespgamelist, goalslist,assistslist,pensmadelist,pensattlist,foulslist,yellowlist,redlist,sotlist]
+#toparse = ["player","position","age","games","game_starts","game_subs","minutes","minutes_per_game"]
 
 # Status notice
 print("Creating dataset")
