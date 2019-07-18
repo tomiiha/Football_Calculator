6398dd44c425ba4d51dbe135f22e460cf0fd6e9d
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-18 10:47:44 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -1,8 +1,9 @@
-# Input for dataset - currently placeholder ifs
-seasons = ["18-19"]
-selection = input('What season?')
-selection = str(selection)
-if selection in seasons:
-    print('Yes')
-else :
-    print('Nah')
+import pandas as pd
+
+# What seasons to include (list off of files)
+seasons_included = ["2017-2018","2018-2019"]
+
+# Load excel sheets per seasons_included
+for season in seasons_included:
+    excel_sheet = pd.read_excel(r'Data\NData' + str(season) + '.xlsx')
+    print(excel_sheet)
