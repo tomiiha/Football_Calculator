7c5f7791e55f3ae4a659691cf2b2c01c7552fafe
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-21 21:27:45 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -3,7 +3,18 @@ import pandas as pd
 # What seasons to include (list off of files)
 seasons_included = ["2017-2018","2018-2019"]
 
+# Lists
+header_list = []
+
 # Load excel sheets per seasons_included
 for season in seasons_included:
     excel_sheet = pd.read_excel(r'Data\NData' + str(season) + '.xlsx')
-    print(excel_sheet)
+    headers = excel_sheet.columns.values.tolist()
+    if headers not in header_list:
+        header_list.append(headers)
+
+# Overview Calcs
+for header in header_list:
+    data_mean = excel_sheet[header].mean()
+    data_median = excel_sheet[header].median()
+    print(round(data_mean,2))
