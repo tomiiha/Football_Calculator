e3e92c824de713624a6d4c1f20ba9abceac8c49a
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-22 10:55:40 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -1,15 +1,14 @@
 import pandas as pd
 
 # What seasons to include (list off of files)
-seasons_included = ["2017-2018","2018-2019"]
+season = "2018-2019"
 
 # Lists
 header_list = []
 
 # Load excel sheets per seasons_included
-for season in seasons_included:
-    excel_sheet = pd.read_excel(r'Data\NData' + str(season) + '.xlsx')
-    headers = excel_sheet.columns.values.tolist()
+excel_sheet = pd.read_excel(r'Data\NData' + str(season) + '.xlsx')
+headers = excel_sheet.columns.values.tolist()
     if headers not in header_list:
         header_list.append(headers)
 
