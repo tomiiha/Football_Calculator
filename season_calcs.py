bd4fee3ce657c148bf70df26ec2cf3ad4921bbe8
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-22 11:27:18 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -8,12 +8,15 @@ header_list = []
 
 # Load excel sheets per seasons_included
 excel_sheet = pd.read_excel(r'Data\NData' + str(season) + '.xlsx')
+excel_sheet = excel_sheet.fillna(value = 0)
 headers = excel_sheet.columns.values.tolist()
-    if headers not in header_list:
-        header_list.append(headers)
+if headers not in header_list:
+    header_list.append(headers)
 
 # Overview Calcs
 for header in header_list:
-    data_mean = excel_sheet[header].mean()
-    data_median = excel_sheet[header].median()
-    print(round(data_mean,2))
+    data_mean = excel_sheet[header][excel_sheet.games > 10].mean()
+    data_mean = round(data_mean,2)
+    data_median = excel_sheet[header][excel_sheet.games > 10].median()
+    data_median = round(data_median,2)
+    print(data_mean)
