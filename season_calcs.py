517b48c14d6a76a053dd76f7fbb4a20453ee0b0b
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-22 14:52:12 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -3,24 +3,13 @@ import pandas as pd
 # What seasons to include (list off of files)
 season_list = ["2017-2018","2018-2019"]
 
-# Exclusion - games column
-excl_value = 10
-
 # Lists
 header_list = []
 
-# Load excel sheets per seasons_included - no data to 0
-for seas in season_list:
-    excel_sheet = pd.read_excel(r'Data\NData' + str(seas) + '.xlsx')
-    excel_sheet = excel_sheet.fillna(value = 0)
-    
-# Load headers from files
-    headers = excel_sheet.columns.values.tolist()
-    if headers not in header_list:
-        header_list.append(headers)
-        
-# Run calcs on individual headers
-    for header in header_list:
-        data_mean = excel_sheet[header][excel_sheet.games >= excl_value].mean()
-        data_mean = round(data_mean,2)
-        print(data_mean)
+# Create series based on file set
+sports_data = [pd.read_excel(r'Data\NData' + str(season) + '.xlsx') for season in season_list]
+for i, season in enumerate(season_list):
+    sports_data[i]['season'] = season
+sports_data = pd.concat(sports_data)
+sports_data = sports_data.groupby('season').mean().T
+print(sports_data)
