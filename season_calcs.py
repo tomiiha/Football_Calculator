891cfcac59c5c3d2629b31d596723bc3aa07a492
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-22 19:01:16 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
 # What seasons to include (list off of files)
-season_list = ["2017-2018","2018-2019"]
-excl_value = 10
-
-# Lists
-header_list = []
+season_list = ["2010-2011", "2011-2012","2012-2013","2013-2014","2014-2015","2015-2016","2016-2017","2017-2018","2018-2019"]
+excl_value = 5
 
 # Create series based on file set
 sports_data = [pd.read_excel(r'Data\NData' + str(season) + '.xlsx') for season in season_list]
@@ -15,4 +12,6 @@ sports_data = pd.concat(sports_data)
 sports_data = sports_data.fillna(value = 0)
 sports_data = sports_data[sports_data.games >= excl_value].groupby('season').mean().T
 sports_data = round(sports_data,3)
-print(sports_data)
+
+# Create Excel sheet with dataframe
+sports_data.to_excel(r'Data\NData BD.xlsx')
