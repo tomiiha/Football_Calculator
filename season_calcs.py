12ba5cf4351e88349d632f453fbb748d9f26184e
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-24 09:35:12 +0100

    Update season_calcs.py

--- season_calcs.py
+++ season_calcs.py
@@ -3,17 +3,16 @@ import glob, os
 
 # What seasons to include (list off of files)
 season_list = []
-os.chdir(r"Data")
+os.chdir(r"C:\Users\tihalainen\Desktop\Data Stuff")
 for file in glob.glob("*.xlsx"):
-    file = file[5:14]
+    file = file[-14:-5]
     season_list.append(str(file))
-print(season_list)
     
 # Min games played
 excl_value = 5
 
 # Create series based on file set
-sports_data = [pd.read_excel(r"Data\Data Northampton Town Season" + str(season) + ".xlsx") for season in season_list]
+sports_data = [pd.read_excel("Data Northampton Town Season " + str(season) + ".xlsx") for season in season_list]
 for i, season in enumerate(season_list):
     sports_data[i]['season'] = season
 sports_data = pd.concat(sports_data)
@@ -23,3 +22,4 @@ sports_data = round(sports_data,3)
 
 # Create Excel sheet with dataframe
 sports_data.to_excel(r'Summary\Summary NorthamptonTown.xlsx')
+print("Summary Created")
