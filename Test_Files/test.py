0cf4fac498a4f60c4fcce304c357d22d3fc6f124
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-29 08:27:54 +0100

    Update test.py

--- Test_Files/test.py
+++ Test_Files/test.py
@@ -1,8 +1,1 @@
-# Get file listing
-import glob, os
-file_list = []
-os.chdir(r"C:\Users\tihalainen\Desktop\Data Stuff\Data")
-for file in glob.glob("*.xlsx"):
-    file = file[5:14]
-    file_list.append(file)
-print(file_list)
+# Nothing under test
