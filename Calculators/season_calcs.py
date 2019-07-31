894060e5fff046924ccc1fde0015918555d76650
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-07-31 21:23:16 +0100

    Update season_calcs.py

--- Calculators/season_calcs.py
+++ Calculators/season_calcs.py
@@ -10,8 +10,9 @@ for file in glob.glob("*.xlsx"):
     file = file[-14:-5]
     season_list.append(str(file))
     
-# Min games played
+# Min games played, and parse list
 excl_value = 5
+to_parse = ["player","position","age","games","games_starts","games_subs","minutes","minutes_per_game","goals","assists","pens_made","pens_att","fouls","cards_yellow","cards_red","shots_on_target"]
 
 # Create series based on file set
 sports_data = [pd.read_excel("Data Northampton Town Season " + str(season) + ".xlsx") for season in season_list]
@@ -21,7 +22,7 @@ sports_data = pd.concat(sports_data)
 sports_data = sports_data.fillna(value = 0)
 
 # Add Over-90 calculations
-sports_data['G/90'] = sports_data['goals'] / (sports_data['minutes'] / 90)
+sports_data['G/90'] = pd.to_numeric(sports_data['goals']) / (pd.to_numeric(sports_data['minutes']) / 90)
 sports_data['A/90'] = sports_data['assists'] / (sports_data['minutes'] / 90)
 sports_data['G+A/90'] = (sports_data['goals'] + sports_data['assists']) / (sports_data['minutes'] / 90)
 sports_data['G-PK/90'] = (sports_data['goals'] - sports_data['pens_made']) / (sports_data['minutes'] / 90)
@@ -35,5 +36,5 @@ sports_data = sports_data[sports_data.games >= excl_value].groupby('season').mea
 sports_data = round(sports_data,3)
 
 # Create Excel sheet with dataframe
-sports_data.to_excel(r'Summary\Summary NorthamptonTown.xlsx')
+sports_data.to_excel(r'Summary\Summary Northampton Town.xlsx')
 print("Summary Created")
