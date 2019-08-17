d8f6d090d803b04665124d0cc9423ccba17fe921
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-08-17 10:47:30 +0100

    Update test.py

--- Test_Files/test.py
+++ Test_Files/test.py
@@ -1,1 +1,98 @@
-# Nothing under test
+
+# Re-code all of how the statisticas are taken - currently it breaks between games, and is super unreliable.
+
+from bs4 import BeautifulSoup as bsoup
+import requests as reqs
+import xlsxwriter as xsl
+
+# Define what team we want the data for
+team_to_scrape = "Northampton Town"
+
+# Season used for file name - nothing fancy
+home_tests = ["https://fbref.com/en/matches/033092ef/Northampton-Town-Lincoln-City-August-4-2018-League-Two", "https://fbref.com/en/matches/e5590f2e/Northampton-Town-Cambridge-United-August-18-2018-League-Two"]
+away_tests = ["https://fbref.com/en/matches/ea736ad1/Carlisle-United-Northampton-Town-August-11-2018-League-Two", "https://fbref.com/en/matches/46c9cbcb/Morecambe-Northampton-Town-August-21-2018-League-Two"]
+
+# Choose home or away game from lists above - indices 0-1
+page_to_parse = home_tests[1]
+
+# Capture website
+page = reqs.get(page_to_parse)
+status_code = page.status_code
+status_code = str(status_code)
+parse_page = bsoup(page.content, 'html.parser')
+
+# Status notice - stat_comp should start with 2 for parsing to have been completed
+stat_comp = "2"
+if status_code[0] == stat_comp:
+    print("Parsing: " + str(page_to_parse) + " completed")
+    print("")
+else:
+    print("There might have been an issue with parsing")
+    print("")
+    
+# Lists
+team_list = []
+score_list = []
+manager_list = []
+foul_list = []
+corner_list = []
+cross_list = []
+touch_list = []
+
+# weekday_list used for date capture
+weekday_list = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+# Capture team names (index 0 is home, index 1 is away)
+team_names = parse_page.find_all("div", itemprop="performer")
+for team in team_names:
+    add_team = team.find_next('a').get_text()
+    team_list.append(str(add_team))
+
+# Team name lengths for data clean-up
+home_len = len(team_list[0])
+away_len = len(team_list[1])
+    
+# Considering all data is split left-right
+# Below chooses the data for the right team per team_to_scrape
+if team_list[0] == team_to_scrape:
+    team_index = 0
+elif team_list[1] == team_to_scrape:
+    team_index = 1
+    
+# Capture game_date
+find_date = parse_page.find("h1")
+add_date = find_date.get_text()
+for date in weekday_list:
+    if add_date.find(date) != -1:
+        date_pos = add_date.find(date)
+        date_len = len(date)
+        game_date = add_date[(date_pos + date_len + 1):]
+
+# Capture game score
+game_scores = parse_page.find_all("div",{"class":"score"})
+for score in game_scores:
+    add_score = score.get_text()
+    score_list.append(int(add_score))
+    
+# Capture managers (remove captains)
+game_manager = parse_page.find_all("div",{"class":"datapoint"})
+for manager in game_manager:
+    add_manager = manager.get_text()
+    if "Captain:" not in add_manager:    
+        manager_list.append(str(add_manager[9:]))
+    
+# Stats are broken down into two divs in the page, list of these elements
+extra_stats = ["Fouls","Corners","Crosses","Touches"]
+
+# Find fouls, add to list
+working_stat = extra_stats[0]
+stat_len = len(working_stat)
+find_fouls = parse_page.find_all('div',id="team_stats_extra")
+for stat in find_fouls:
+    add_foul = stat.find_next('div').get_text()
+    add_foul = add_foul[(home_len + away_len - 2):]
+    add_foul = add_foul[:add_foul.find('\n')]
+    add_foul_home = add_foul[:add_foul.find(working_stat)]
+    add_foul_away = add_foul[(add_foul.find(working_stat) + stat_len):]
+    foul_list.append(int(add_foul_home))
+    foul_list.append(int(add_foul_away))
