4c3d82c4348883694b3b8c0e804f832404a6635b
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-09-07 15:18:39 +0100

    Update match_list_scraper.py

--- Scraping_Tools/match_list_scraper.py
+++ Scraping_Tools/match_list_scraper.py
@@ -1,23 +1,22 @@
 # Note: https://fbref.com/robots.txt
-# Start with https://fbref.com/en/squads/986a26c1/2018-2019/Northampton-Town
-# To be combined fully with game_scraper.py
+# Start with https://fbref.com/en/squads/986a26c1/Northampton-Town
 
 from bs4 import BeautifulSoup as bsoup
 import requests as reqs
 
 # Season used for file name - nothing fancy
-page_to_parse = 'https://fbref.com/en/squads/986a26c1/2018-2019/Northampton-Town'
+season_to_parse = 'https://fbref.com/en/squads/986a26c1/2018-2019/Northampton-Town'
 
 # Capture website
-page = reqs.get(page_to_parse)
+page = reqs.get(season_to_parse)
 status_code = page.status_code
 status_code = str(status_code)
-parse_page = bsoup(page.content, 'html.parser')
+season_page = bsoup(page.content, 'html.parser')
 
 # Status notice - stat_comp should start with 2 for parsing to have been completed
 stat_comp = "2"
 if status_code[0] == stat_comp:
-    print("Parsing: " + str(page_to_parse) + " completed")
+    print("Parsing: " + str(season_to_parse) + " completed")
     print("")
 else:
     print("There might have been an issue with parsing")
@@ -26,7 +25,7 @@ else:
 match_list= []
 
 # Capture all match URLs from the main season
-findinfo = parse_page.find_all('td',attrs={"data-stat":"match_report"})
+findinfo = season_page.find_all('td',attrs={"data-stat":"match_report"})
 for datum in findinfo:
     add_datum = datum.find_next('a').attrs['href']
     full_link = "https://fbref.com" + str(add_datum)
