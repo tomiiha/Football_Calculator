a7836f4c0e5424af86238bd852533945d77f4a63
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-09-09 21:20:21 +0100

    Update test.py

--- Test_Files/test.py
+++ Test_Files/test.py
@@ -1,9 +1,10 @@
 team_list = ['Carlisle United','Northampton']
-removal = str(team_list[0] + "\\xa0" + team_list[1])
+index_list = []
+removal = team_list[0] + team_list[1]
 extra_stats = ["Fouls","Corners","Crosses","Touches","Clearances","Offsides","Goal Kicks","Throw Ins","Long Balls"]
 text = '\n\nCarlisle United\xa0Northampton\n21Fouls12\n4Corners2\n10Crosses7\n83Touches101\n\n\nCarlisle United\xa0Northampton\n1Clearances1\n\n\nCarlisle United\xa0Northampton\n10Goal Kicks12\n16Throw Ins18\n15Long Balls17\n\n'
+text = text.replace(u'\xa0', u'')
 text = text.split('\n')
 text = list(filter(None, text))
-while removal in text: text = text.remove(removal)
-
-print(text)
+while removal in text:
+    text.remove(removal)
