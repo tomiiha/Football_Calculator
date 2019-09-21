8f48c2eaaf57708895ef41cef436c9a03cddb567
tomiiha <52747351+tomiiha@users.noreply.github.com>
2019-09-21 09:35:35 +0100

    Update test.py

--- Test_Files/test.py
+++ Test_Files/test.py
@@ -8,15 +8,14 @@ other_stat_list = []
 
 page_to_parse = 'https://fbref.com/en/matches/033092ef/Northampton-Town-Lincoln-City-August-4-2018-League-Two'
     
-page = reqs.get(season_to_parse)
+page = reqs.get(page_to_parse)
 status_code = page.status_code
 status_code = str(status_code)
 parse_page = bsoup(page.content, 'html.parser')
 
-find_other_stats = parse_page.find_all('div', id="team_stats")
-all_other_stats = find_other_stats[0].find_all('div', recursive=False)
-for stat in all_other_stats:
-    add_other_stats = find_next('strong').get_text()
-     other_stat_list.append(add_team)
 
-print(other_stat_list)
+find_other_stats = parse_page.find_all('div', id="team_stats")
+for stat in find_other_stats:
+    add_other_stats = stat.find_next('strong').get_text()
+    other_stat_list.append(add_other_stats)
+    print(add_other_stats)
