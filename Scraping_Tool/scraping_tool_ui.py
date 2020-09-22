17c57b2c5788da31f5e90ea168982e578abbf483
tomiiha <52747351+tomiiha@users.noreply.github.com>
2020-09-22 11:06:55 +0100

    Update scraping_tool_ui.py

--- Scraping_Tool/scraping_tool_ui.py
+++ Scraping_Tool/scraping_tool_ui.py
@@ -1,3 +1,5 @@
+import ipywidgets as widgets
+
 # Capture files for processing
 def initial_load():
     import os
@@ -6,14 +8,18 @@ def initial_load():
                  if data_file.endswith('.json')]
     with open(data_filename[0]) as json_file:
             data = json.load(json_file)
-    return league_picker(data)
+    return league_select(data)
 
 # Pick league.
-def league_picker(data):
+def league_select(data):
     league_list = []
     for y in data:
         if y['Competition'] is not None and y['Competition'] not in league_list:
             league_list.append(y['Competition'])
+    return team_list_create(league_list, data)
+
+# Pull league teams per selection
+def team_list_create(league_list, data):
     league_choice = league_list[1]
     team_list = []
     for x in data:
