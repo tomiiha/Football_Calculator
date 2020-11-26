c0048f7477dcd2ba3017150649a4912d5df5b582
Tomi <52747351+tomiiha@users.noreply.github.com>
2020-11-26 14:17:32 +0000

    Create mongodb_conn.py

---
+++ API/mongodb_conn.py
@@ -0,0 +1,26 @@
+mport pymongo
+import ast
+
+def conn_establish():
+    # Pass credentials out of credentials file.
+    credentials = ast.literal_eval(open('credentials.txt','r').read())
+    conn = "mongodb+srv://" + credentials['username'] + ":" + credentials['password'] + "@sandbox.r1tr0.mongodb.net/" + credentials['db_name'] + "?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(conn)
+    return conn_test(client)
+
+# Run connection - test and run, otherwise close connection straight.
+def conn_test(client):
+    try:
+        client.server_info()
+        return print("Connection Established")
+    except:
+        print("Connection failed")
+        close_conn(client)
+
+# Terminate connection.
+def close_conn(client):
+    try:
+        client.close()
+        return
+    except:
+        return print("No Connection Established")
