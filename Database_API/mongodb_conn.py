f664f7513bd213fcdac7df9eac9e50bec892905f
Tomi <52747351+tomiiha@users.noreply.github.com>
2020-11-27 12:48:13 +0000

    Update mongodb_conn.py

--- Database_API/mongodb_conn.py
+++ Database_API/mongodb_conn.py
@@ -4,8 +4,8 @@ import ast
 def conn_establish():
     # Pass credentials out of credentials file.
     credentials = ast.literal_eval(open('credentials.txt','r').read())
-    conn = "mongodb+srv://" + credentials['username'] + ":" + credentials['password'] + "@sandbox.r1tr0.mongodb.net/" + credentials['db_name'] + "?retryWrites=true&w=majority"
-    client = pymongo.MongoClient(conn)
+    conn_url = "mongodb+srv://" + credentials['username'] + ":" + credentials['password'] + "@sandbox.r1tr0.mongodb.net/" + credentials['db_name'] + "?retryWrites=true&w=majority"
+    client = pymongo.MongoClient(conn_url)
     return conn_test(client)
 
 # Run connection - test and run, otherwise close connection straight.
